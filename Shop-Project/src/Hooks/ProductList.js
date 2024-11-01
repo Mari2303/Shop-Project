7a08b886cdc5714d2f18c392fd8c25d5d28cc893
Maria2303 <mariaalejandramarinhenriquez@gmail.com>
2024-11-01 14:47:39 -0500

    organizacion de estilos de la pagina

--- Shop-Project/src/Hooks/ProductList.js
+++ Shop-Project/src/Hooks/ProductList.js
@@ -1,12 +1,24 @@
-import React from 'react';
-
-const ProductList = () => {
-    return (
-        <div>
-            <h1>Lista de Productos</h1>
-            {/* Aquí agregarás la lógica para mostrar tus productos */}
-        </div>
-    );
-};
-
-export default ProductList;
+async function fetchProducts() {
+    try {
+      const response = await fetch("URL_DE_LA_API");
+      const products = await response.json();
+      const productList = document.getElementById("product-list");
+  
+      products.forEach((product) => {
+        const productItem = document.createElement("div");
+        productItem.classList.add("product-item");
+        productItem.innerHTML = `
+          <img src="${product.image}" alt="${product.name}">
+          <h4>${product.name}</h4>
+          <p>${product.description}</p>
+          <p>${product.price}</p>
+        `;
+        productList.appendChild(productItem);
+      });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
+  
+  fetchProducts();
+  
